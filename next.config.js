f209ee8f3da56336273a95028f95b84faf01dfd5
kiminozo <kiminozo.eien@gmail.com>
2019-12-03 18:01:09 +0800

    init

---
+++ next.config.js
@@ -0,0 +1,16 @@
+const withCss = require('@zeit/next-css')
+const withSass = require('@zeit/next-sass')
+const withLess = require('@zeit/next-less')
+
+const isProd = process.env.NODE_ENV === 'production'
+
+// fix: prevents error when .less files are required by node
+if (typeof require !== 'undefined') {
+    require.extensions['.less'] = file => {}
+}
+
+module.exports = withLess(withSass(withCss({
+    lessLoaderOptions: {
+        javascriptEnabled: true
+    }
+})))
---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default () => (
+    <ul>
+        <li><Link href='/app' as='/app'><a>app</a></Link></li>
+        <li>
+            <Link
+                href={{ pathname: '/posts', query: { id: '2' } }}
+                as='/posts/2'
+            >
+                <a>post #2</a>
+            </Link>
+        </li>
+    </ul>
+)
